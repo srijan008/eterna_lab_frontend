168b3c2c42f14e3b239c9c3e71dd79f1206cacaa
srijan008 <srijankumar008@gmail.com>
2025-11-28 04:33:59 +0530

    fix bug

---
+++ src/app/pulse/page.tsx
@@ -0,0 +1,9 @@
+import { TokenTable } from "@/components/token-table/TokenTable";
+
+export default function PulsePage() {
+  return (
+    <div className="w-full">
+      <TokenTable />
+    </div>
+  );
+}
